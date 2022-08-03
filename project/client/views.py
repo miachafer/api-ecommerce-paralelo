1221c6adcf8e36fda0bf9725cb9d6f47cbcb0e77
miachafer <miachafer@gmail.com>
2022-08-03 13:25:28 -0300

    Viewset final version for client app

--- project/client/views.py
+++ project/client/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Client
 from .serializers import ClientSerializer
-from rest_framework.decorators import action
 
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
-    
