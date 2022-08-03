2d746b5404c6d876a90ac3a1ed8755b26bfa2ccc
miachafer <miachafer@gmail.com>
2022-08-03 15:44:54 -0300

    Add validator to description field.

--- project/order/serializers.py
+++ project/order/serializers.py
@@ -1,47 +1,21 @@
 from rest_framework import serializers
 from .models import Order, Item
-# from project.product.serializers import ProductSerializer
-# from project.product.models import Product
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = '__all__'
 
+    """
+    The following function validate if the order's description contains letters. If the description doesn't contain a single letter, these function will raise an error.
+    """
+    def validate_description(self, description):
+        is_there_a_letter = any(c.isalpha() for c in description)
+        if is_there_a_letter == False:
+            raise serializers.ValidationError("The order's description must contain letters.")
+        return description
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__'
-    
-
-    
-    # def create(self, validated_data):
-    #     products_data = validated_data.pop('products')
-    #     order = Order.objects.create(**validated_data)
-    #     for product_data in products_data:
-    #         Product.objects.create(order=order, **product_data)
-    #     return order
-
-'''
-{
-        "id": 5,
-        "products": [
-            {
-                "id": 2,
-                "name": "Caneta",
-                "description": "Caneta preta.",
-                "price": 1,
-                "quantity": 10 ### THIS CAN'T SHOW!
-            },
-            {
-                "id": 3,
-                "name": "Borracha",
-                "description": "Borracha verde",
-                "price": 2,
-                "quantity": 10 ### THIS CAN'T SHOW!
-            }
-        ],
-        "client": "11122233345"
-    },
-'''
