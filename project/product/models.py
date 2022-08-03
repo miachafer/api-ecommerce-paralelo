d06c5af383a2f5ff86cb51757873c4ee8238ef43
miachafer <miachafer@gmail.com>
2022-08-03 14:42:13 -0300

    Update field to blank=True and add validation to negative numbers

--- project/product/models.py
+++ project/product/models.py
@@ -1,11 +1,21 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.IntegerField(default=0)
-    quantity = models.IntegerField(default=0)
+    name = models.CharField(max_length=255, blank=False)
+    description = models.TextField(blank=False)
+    price = models.DecimalField(
+        default=0.00, 
+        max_digits=10, 
+        decimal_places=2,
+        blank=False,
+        validators=[MinValueValidator(0.01)],
+        help_text="Type the price in the format: 0.00")
+    quantity = models.IntegerField(
+        default=0,
+        blank=False,
+        validators=[MinValueValidator(0)])
 
     def __str__(self):
-        return f'{self.name},\n{self.description},\n{self.price}'
+        return f"{self.name} - {self.description} - ${self.price} - {self.quantity} available"
     
