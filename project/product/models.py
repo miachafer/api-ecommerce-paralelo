54e97852e7dd495942a9c24c85af6dcaac77ad60
miachafer <miachafer@gmail.com>
2022-08-03 17:00:54 -0300

    Comment default values to numeric fields

--- project/product/models.py
+++ project/product/models.py
@@ -5,17 +5,17 @@ class Product(models.Model):
     name = models.CharField(max_length=255, blank=False)
     description = models.TextField(blank=False)
     price = models.DecimalField(
-        default=0.00, 
+        # default=0.00, 
         max_digits=10, 
         decimal_places=2,
         blank=False,
         validators=[MinValueValidator(0.01)],
         help_text="Type the price in the format: 0.00")
     quantity = models.IntegerField(
-        default=0,
+        # default=0,
         blank=False,
         validators=[MinValueValidator(0)])
 
     def __str__(self):
-        return f"{self.name} - {self.description} - ${self.price} - {self.quantity} available"
+        return f"{self.id} - {self.name} - {self.description} - ${self.price} - {self.quantity} available"
     
