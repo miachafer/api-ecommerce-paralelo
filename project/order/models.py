17971b1212fed825c7c58b42ec8533341ed26c57
miachafer <miachafer@gmail.com>
2022-08-03 15:43:52 -0300

    Update models and add validators

--- project/order/models.py
+++ project/order/models.py
@@ -1,21 +1,26 @@
 from django.db import models
 from project.client.models import Client
 from project.product.models import Product
+from django.core.validators import MinValueValidator
 
 class Order(models.Model):
-    description = models.TextField()
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    description = models.TextField(blank=False)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.id} - {self.description}'
+        return f"Order: {self.id} - Description: {self.description}"
 
 class Item(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
-    quantity = models.IntegerField(default=0, null=False)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, blank=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=False)
+    quantity = models.IntegerField(
+        # default = 0,
+        blank=False,
+        validators=[MinValueValidator(0)])
 
     def __str__(self):
-        return f'Order: {self.order_number} - Client: {self.client} - Product: {self.product} - Quantity: {self.quantity}'
+        return f"{self.id} - Order_related: {self.order} - Product: {self.product} - Quantity: {self.quantity}"
 
     
