9c86c7235cef9fffa58bc03a665f82a0f2c229c7
miachafer <miachafer@gmail.com>
2022-08-03 14:42:47 -0300

    Add validation to product's name and description

--- project/product/serializers.py
+++ project/product/serializers.py
@@ -5,3 +5,19 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+    """
+    The following functions validate if the product's name and description contains letters. If the name and/or description don't contain letter, these function will raise an error.
+    """
+    def validate_name(self, name):
+        is_there_a_letter = any(c.isalpha() for c in name)
+        if is_there_a_letter == False:
+            raise serializers.ValidationError("The product's name must contain letters.")
+        return name
+    
+    def validate_description(self, description):
+        is_there_a_letter = any(c.isalpha() for c in description)
+        if is_there_a_letter == False:
+            raise serializers.ValidationError("The product's description must contain letters.")
+        return description
+        
