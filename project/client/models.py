be2519737f3b90d78cceb087b835edc440841a38
miachafer <miachafer@gmail.com>
2022-08-03 13:22:35 -0300

    Update model phone number

--- project/client/models.py
+++ project/client/models.py
@@ -1,43 +1,30 @@
 from django.db import models
 from django.core.validators import RegexValidator
+from localflavor.br.models import BRCPFField
 
-'''
-This phone regex validates Brazil's cell phones and landlines.
-Both types of phone numbers have an area code of 2 digits.
-Cell phone numbers have 9 digits, and always start with 9.
-Landlines number have 8 digits, and never start with 9.
-'''
-
-PHONE_REGEX = RegexValidator(r'^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$', 'Enter a phone number in the correct format. Mobile phones: (XX) XXXXX-XXXX Landlines: (XX) XXXX-XXXX')
-
-'''
+"""
 The primary key of this model is the CPF (Cadastro de Pessoa Física), or the Brazilian equivalent to Social Security Number.
-The CPF number is going to be validated by the CPF_REGEX, which validates the format XXX.XXX.XXX-XX
-'''
-
-# CPF_REGEX = RegexValidator(r'^[1-9]{3}\.[1-9]{3}\.[1-9]{3}\-[1-9]{2}$', 'Enter your CPF number in the format XXX.XXX.XXX-XX')
+The CPF number is going to be validated by the library django-localflavor.
+"""
 
 class Client(models.Model):
     name = models.CharField(
-        max_length=100,
+        max_length=255,
         verbose_name="Full name",
         help_text="Type a name and a surname")
-    cpf = models.CharField(
-        primary_key=True,
-        max_length=14,
-        verbose_name="CPF",
-        help_text="Type your CPF in the following format: 11122233344",
-        )
+    cpf = BRCPFField( 
+        max_length=11, 
+        blank=False, 
+        primary_key=True, 
+        help_text="Type your CPF in the following format: 11122233344")
     address = models.TextField(verbose_name="Address")
     phone = models.CharField(
-        max_length=15,
+        max_length=11,
         verbose_name="Phone number",
-        help_text="Type phone number in the following format: (XX) XXXXX-XXXX for mobile phones or (XX) XXXX-XXXX for landlines.",
-        validators=[PHONE_REGEX],
-        unique=True)
-    email = models.EmailField(max_length=100, unique=True)
-    # created_at = models.DateTimeField(auto_now_add=True)
-    # updated_at = models.DateTimeField(auto_now=True)
+        help_text="Type phone number in the following format: DDD (2 digits) + number (8 or 9 digits) with no spaces")
+    email = models.EmailField(max_length=256, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.name}, CPF: {self.cpf}'
+        return f"Client: {self.name} - CPF: {self.cpf}"
